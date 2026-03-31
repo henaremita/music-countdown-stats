b3872a006d164b02e38f4d057f5f9adb12df3a9e
henaremita <84121150+henaremita@users.noreply.github.com>
2026-03-31 15:40:26 +1300

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.title("🎵 Live Song Countdown Dashboard")
 conn = st.connection("gsheets", type=GSheetsConnection)
 
 # 2. Read Data (Use your Sheet URL here)
-url = "https://docs.google.com/spreadsheets/d/1m4evkbKGu45kPenBkj6_8rwvpuhIfL6BTc1S1I4wi28/edit?gid=1189458849#gid=1189458849"
+url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQJhiV_Pa-naVPS--8plhf4I7Qh0HdH4mOVl4D2bql-fe87W5SN1wxwB52Bo1d_Q4yd1eC6RdXPiBez/pub?output=csv"
 df = conn.read(spreadsheet=url, ttl="10m") # Updates every 10 mins
 
 # 3. Top Level Metrics
